5c23889088de301b910f0241b91218b450cac5d0
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2024-06-03 17:57:42 +0300

    more delete

--- module_1/hwOne/index.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-const nameProduct = "apple";
-const qualityProduct = 3;
-const categoryProduct = "fruits";
-const priceProduct = 5;
-
-console.log("Наименование товара: ", nameProduct);
-console.log("Общая сумма товара: ", qualityProduct * priceProduct);
