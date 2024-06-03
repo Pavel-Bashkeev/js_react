485573e9841597220daecded85b9fcac25900b88
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2024-06-03 18:05:47 +0300

    make hw

---
+++ module_1/topic_2/hw/index.js
@@ -0,0 +1,19 @@
+"use strict";
+{
+  const nameProduct = "Яблоко";
+  const qualityProduct = 3;
+  const categoryProduct = "Фрукты";
+  const priceProduct = 5;
+
+  console.log("Наименование товара: ", nameProduct);
+  console.log("Общая сумма товара: ", qualityProduct * priceProduct);
+}
+{
+  const nameProduct = "Огурец";
+  const qualityProduct = 10;
+  const categoryProduct = "Овощи";
+  const priceProduct = 50;
+
+  console.log("Наименование товара: ", nameProduct);
+  console.log("Общая сумма товара: ", qualityProduct * priceProduct);
+}
