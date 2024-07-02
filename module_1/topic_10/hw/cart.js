ee68472ac384d5cc3a754a4ea266aeef41bc08e8
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2024-07-03 00:14:47 +0300

    hw11

--- module_1/topic_10/hw/cart.js
+++ module_1/topic_10/hw/cart.js
@@ -1,38 +1,38 @@
-"use strict";
+'use strict';
 
 const cart = {
-	items: [],
-	count: 0,
-	get totalPrice() {
-		return this.calculateItemPrice();
-	},
-	add: function(name, price, amount = 1) {
-		const newProduct = {name, price, amount};
-		this.items.push(newProduct);
-		this.increaseCount(amount);
-	},
-	increaseCount: function (amount) {
-		this.count += amount;
-		return this.count;
-	},
-	calculateItemPrice: function () {
-		return this.items.reduce((allTotal, item) => allTotal + item.price * item.amount, 0);
-	},
-	clear: function() {
-		this.items = [];
-		this.totalPrice = 0;
-		this.count = 0;
-		return true;
-	},
-	print: function() {
-		if(this.items.length){
-			console.log(`Корзина: ${JSON.stringify(this.items, null, 2)}`);
-			console.log(`Общая сумма корзины: ${new Intl.NumberFormat("ru").format(this.totalPrice)} деревянных!`);
-			return true;
-		}
-		console.log(`Ваша корзина пуста`);
-		return true;
-	},
+  items: [],
+  count: 0,
+  get totalPrice() {
+    return this.calculateItemPrice();
+  },
+  add(name, price, amount = 1) {
+    const newProduct = {name, price, amount};
+    this.items.push(newProduct);
+    this.increaseCount(amount);
+  },
+  increaseCount(amount) {
+    this.count += amount;
+    return this.count;
+  },
+  calculateItemPrice() {
+    return this.items.reduce((sum, item) => sum + item.price * item.amount, 0);
+  },
+  clear() {
+    this.items = [];
+    this.count = 0;
+    return true;
+  },
+  print() {
+    if (this.items.length) {
+      const formatPrice = new Intl.NumberFormat('ru').format(this.totalPrice);
+      console.log(`Корзина: ${JSON.stringify(this.items, null, 2)}`);
+      console.log(`Общая сумма корзины: ${formatPrice} деревянных!`);
+      return true;
+    }
+    console.log(`Ваша корзина пуста`);
+    return true;
+  },
 };
 
 cart.add('Товар 4', 300);
