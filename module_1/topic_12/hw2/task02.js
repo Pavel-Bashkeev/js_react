ced6bb0db2b6c044f31ed016759d0bc0f23b5693
Pavel-Bashkeev <pavelbashkeev@gmial.com>
2024-07-17 11:49:43 +0300

    hw12

--- module_1/topic_12/hw2/task02.js
+++ module_1/topic_12/hw2/task02.js
@@ -3,8 +3,18 @@
 const cart = {
 	items: [],
 	count: 0,
+	discount: 0,
 	get totalPrice() {
 		return this.calculateItemPrice();
+	},
+	set setDiscount(promocode) {
+		if (promocode === 'METHED') {
+			this.discount = 15;
+		} else if (promocode === 'NEWYEAR') {
+			this.discount = 21;
+		} else {
+			this.discount = 0;
+		}
 	},
 	add(name, price, amount = 1) {
 		const newProduct = {name, price, amount};
@@ -16,11 +26,13 @@ const cart = {
 		return this.count;
 	},
 	calculateItemPrice() {
-		return this.items.reduce((sum, item) => sum + item.price * item.amount, 0);
+		const total = this.items.reduce((sum, item) => sum + item.price * item.amount, 0);
+		return total * (1 - this.discount / 100);
 	},
 	clear() {
 		this.items = [];
 		this.count = 0;
+		this.discount = 0;
 		return true;
 	},
 	print() {
@@ -38,5 +50,7 @@ const cart = {
 cart.add('Товар 4', 300);
 cart.add('Товар 5', 150, 2);
 cart.add('Товар 6', 450, 3);
+
+cart.setDiscount = 'METHED';
 
 cart.print();
